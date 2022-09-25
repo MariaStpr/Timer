f56518a86036805b44af1a2dd63b0cee58669744
MariaStpr <stepuramasha1234@gmail.com>
2022-09-25 19:46:30 +0800

    без webpack

--- src/index.js
+++ src/index.js
@@ -1,66 +1,44 @@
-<<<<<<< HEAD:src/index.js
 import Timer from './class.js';
-import '../style.css';
-=======
-'use strict'
->>>>>>> parent of 772b924 (eslint):index.js
 
-let titleDate = document.querySelector('#title-date'); //Заголовок
-let date = document.querySelector('#date'); //Дата
-const startBtn = document.querySelector('#btn'); //Кнопка "начать"
-const resetBtn = document.querySelector('.btn-reset'); //Кнопка "сбросить"
-let titleComplete = document.querySelector('.complete');
-let time = document.querySelector('.numbers');
-
+const titleDate = document.querySelector('#title-date'); // Заголовок
+const date = document.querySelector('#date'); // Дата
+const startBtn = document.querySelector('#btn'); // Кнопка "начать"
+const resetBtn = document.querySelector('.btn-reset'); // Кнопка "сбросить"
+const titleComplete = document.querySelector('.complete');
+const time = document.querySelector('.numbers');
+const timerTitle = document.querySelector('h1');
+const timerInput = document.querySelector('.input');
+const timerOutput = document.querySelector('.output');
 let futureTime = '';
-let timerId;
+let timer;
 
-
-function roundTime(time) {
-    if (time < 10) {
-        return '0' + time;
-    } else {
-        return time;
-    }
+function showFinishMessage(dateEnd) {
+    titleComplete.classList.remove('hide');
+    titleComplete.textContent = `${titleDate.value} завершился ${dateEnd}`;
 }
 
-function countDiff() {
-    const now = new Date().getTime();
-    let diff = new Date(futureTime).getTime() - now;
-    
-    if (diff < 0) {
-        
-        document.querySelector('.output').classList.add('hide');
-        titleComplete.classList.remove('hide');
+function showNextView() {
+    // отобразить окно с таймером
+    timerInput.classList.add('hide');
+    timerOutput.classList.remove('hide');
+    resetBtn.classList.remove('hide');
+    startBtn.classList.add('hide');
+  }
 
-        clearInterval(timerId);
-
-        titleComplete.textContent = `${titleDate.value} завершился ${futureTime}`;
-    
-        return;
-    }
-    
-    let seconds = Math.floor((diff/1000)%60);
-    let minutes = Math.floor((diff/(1000*60))%60);
-    let hours = Math.floor((diff/(1000*60*60))%24);
-    let days = Math.floor(diff/(1000*60*60*24));
-
-    time.textContent = `${roundTime(days)}:${roundTime(hours)}:${roundTime(minutes)}:${roundTime(seconds)}`;
-}
-    
-function startDate() {
+  function startDate() {
+    timer = new Timer(date.value, time, showFinishMessage);
+    timer.callCountDiff();
     futureTime = date.value;
-
     if (futureTime === '') {
         alert('Пожалуйста, введите дату');
         return;
-    } else if (isNaN(Date.parse(futureTime))) { //валидность даты
+      }
+      if ((Date.parse(futureTime).isNaN)) { // валидность даты
         alert('Введите корректную дату');
         return;
-    }
-
-    //перенести заголовок и разницу во времени в окно с таймером
-    document.querySelector('h1').textContent = titleDate.value;
+      }
+    // перенести заголовок и разницу во времени в окно с таймером
+    timerTitle.textContent = titleDate.value;
 
     localStorage.setItem('title', titleDate.value);
     localStorage.setItem('date', date.value);
@@ -68,51 +46,38 @@ function startDate() {
     showNextView();
 }
 
-function showNextView() {
-    //отобразить окно с таймером
-    document.querySelector('.input').classList.add('hide');
-    document.querySelector('.output').classList.remove('hide');
-    resetBtn.classList.remove('hide');
-    startBtn.classList.add('hide');
-
-    countDiff();
-    timerId = setInterval(countDiff, 1000);
-}
-
 function clear() {
-    document.querySelector('h1').textContent = 'Создать новый таймер обратного отсчета';    
-    titleDate.value = '';
-    date.value = '';
+  timerTitle.textContent = 'Создать новый таймер обратного отсчета';
+  titleDate.value = '';
+  date.value = '';
 
-    document.querySelector('.input').classList.remove('hide');
-    document.querySelector('.output').classList.add('hide');
-    resetBtn.classList.add('hide');
-    startBtn.classList.remove('hide');
-    titleComplete.classList.add('hide');
+  timerInput.classList.remove('hide');
+  timerOutput.classList.add('hide');
+  resetBtn.classList.add('hide');
+  startBtn.classList.remove('hide');
+  titleComplete.classList.add('hide');
 
-    clearInterval(timerId);
-
-    localStorage.removeItem("title");
-    localStorage.removeItem("date");
+  timer.clearInterval();
+  localStorage.removeItem('title');
+  localStorage.removeItem('date');
 }
 
 function local() {
-    const titleTimer = localStorage.getItem('title');
-    const dateTimer = localStorage.getItem('date');
+  const titleTimer = localStorage.getItem('title');
+  const dateTimer = localStorage.getItem('date');
 
-    if (!(titleTimer && dateTimer)) {
-        return;
-    }
+  if (!(titleTimer && dateTimer)) {
+    return;
+  }
+  timer = new Timer(dateTimer, time, showFinishMessage);
+  timer.callCountDiff();
 
-    document.querySelector('h1').textContent = titleTimer;
-    futureTime = dateTimer;
-
-    showNextView();
+  timerTitle.textContent = titleTimer;
+  showNextView();
 }
 
-//При клике на startBtn
+// При клике на startBtn
 startBtn.addEventListener('click', startDate);
 
 resetBtn.addEventListener('click', clear);
-
 local();
