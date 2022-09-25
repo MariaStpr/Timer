4b60e4f7167c9fee2c1016acff3828e1eaee4293
MariaStpr <stepuramasha1234@gmail.com>
2022-09-25 18:55:59 +0800

    work

--- src/class.js
+++ src/class.js
@@ -1,4 +1,5 @@
 export default class Timer {
+<<<<<<< HEAD:src/class.js
   constructor(futureTime, time, showFinishMessage) {
     this.date = futureTime;
     this.timerId = null;
@@ -25,19 +26,44 @@ export default class Timer {
       clearInterval(this.timerId);
 
       return;
+=======
+    constructor (futureTime, time, showFinishMessage) {
+        this.date = futureTime;
+        this.timerId = null;
+        this.timeNumbers = time;
+        this.finishMessage = showFinishMessage;
+>>>>>>> parent of 772b924 (eslint):class.js
     }
-    this.showCountDown(diff);
-  }
+    callCountDiff(timer) {
+        this.countDiff(timer);
+        this.timerId = setInterval(this.countDiff.bind(this), 1000);
+    }
+    countDiff() {
+        const now = new Date().getTime();
+        const diff = new Date(this.date).getTime() - now;
+        
+        if (diff < 0) {
+            this.timeNumbers.textContent = `00:00:00:00`;
+            this.finishMessage(this.date);
 
-  showCountDown(diff) {
-    const seconds = Math.floor((diff / 1000) % 60);
-    const minutes = Math.floor((diff / (1000 * 60)) % 60);
-    const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
-    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+            localStorage.removeItem('title');
+            localStorage.removeItem('date');
+    
+            clearInterval(this.timerId);
 
-    this.timeNumbers.textContent = `${this.roundTime(days)}:${this.roundTime(hours)}:${this.roundTime(minutes)}:${this.roundTime(seconds)}`;
-  }
+            return;
+        }
+        this.showCountDown(diff);
+    }
+    showCountDown(diff) {
+        const seconds = Math.floor((diff/1000)%60);
+        const minutes = Math.floor((diff/(1000*60))%60);
+        const hours = Math.floor((diff/(1000*60*60))%24);
+        const days = Math.floor(diff/(1000*60*60*24));
+    
+        this.timeNumbers.textContent = `${this.roundTime(days)}:${this.roundTime(hours)}:${this.roundTime(minutes)}:${this.roundTime(seconds)}`;
 
+<<<<<<< HEAD:src/class.js
   clearInterval() {
     clearInterval(this.timerId);
   }
@@ -46,3 +72,17 @@ export default class Timer {
     return time < 10 ? `0${time}` : time;
   }
 }
+=======
+    }
+    clearInterval() {
+        clearInterval(this.timerId);
+    }
+    roundTime(time) {
+        if (time < 10) {
+            return '0' + time;
+        } else {
+            return time;
+        }
+    }
+}
+>>>>>>> parent of 772b924 (eslint):class.js
